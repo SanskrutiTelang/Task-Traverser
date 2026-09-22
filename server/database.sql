702b829a9a162f8bc551783fec1f772fc715345c
Sanskruti Telang <sanskrutitelang740@gmail.com>
2026-09-22 23:50:22 +0530

    adding all the files and .env

--- server/database.sql
+++ server/database.sql
@@ -1,5 +1,14 @@
+-- CREATE DATABASE todotraverse;
+-- CREATE TABLE todo(
+--     todo_id SERIAL PRIMARY KEY,
+--     description VARCHAR(30)
+-- );
+
 CREATE DATABASE todotraverse;
-CREATE TABLE todo(
+
+\c todotraverse
+
+CREATE TABLE todo (
     todo_id SERIAL PRIMARY KEY,
     description VARCHAR(30)
 );
